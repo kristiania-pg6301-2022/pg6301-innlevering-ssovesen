740d66908417c84e1930ef350dbd2f43deb538eb
orjanskotnes <oskotnes@gmail.com>
2022-02-17 13:36:50 +0100

    "should list all todos" FAILS

--- __tests__/todoServer.test.jsx
+++ __tests__/todoServer.test.jsx
@@ -14,4 +14,14 @@ describe("Todo app server testing", () => {
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject({ message: "This is all" });
   });
+
+  it("should list all todos", async () => {
+    const response = await request(app).get("/api/getAll");
+    expect(response.body).toMatchObject({
+      id: expect.any(Number),
+      title: expect.any(String),
+      text: expect.any(String),
+      finished: expect.any(Boolean),
+    });
+  });
 });
